368ab9c3473b62dc8a5923eea571073ed56f1ca9
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-08 10:08:20 -0600

    listed the list function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -6,8 +6,21 @@ let basket = [];
 
 function addItem(item) {
     basket.push(item);
-    console.log('New item going into the basket:', item);
+    console.log('New item added to the basket:', item);
     return true;
 }
 
 console.log(addItem('socks'));
+console.log(addItem('t-shirt'));
+console.log(addItem('jeans'));
+console.log(addItem('undies'));
+
+
+function listItems(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+    return 'Items in list';
+}
+
+console.log(listItems(basket));
