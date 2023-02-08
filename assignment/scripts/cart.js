5028a505be26629a279aa14e5738f2a73dae0970
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-08 11:07:11 -0600

    empty basket function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -24,13 +24,52 @@ console.log(addItem('undies'));
 
 function listItems(array) {
     for (let i = 0; i < array.length; i++) {
-        console.log(array[i]);
+        console.log('Item in basket:', array[i]);
     }
-    return 'Items in list';
+    return true;
 }
 
 console.log(listItems(basket));
 
 //Create a function called 'empty'
 
+function empty(array) {
+    array.length = 0;
+    array.splice(0, array.length);
+    console.log('Basket is empty', array);
+    return true;
+}
+console.log(empty(basket));
+
+
+
+
+
+//Ways I tried first to make 'empty' function:
+
+//function empty(item){
+//  basket.pop(item);
+//  console.log('Removed all items:', item);
+//  return 'Empty Basket';
+//}
+//console.log(empty(basket));
+
+//function empty(item) {
+  //  for(i=0; i<item.lenght; i++){
+    //    basket.pop(item);
+      //  console.log('Removed item from basket:', item);
+    //}
+//}
+//console.log(empty(basket));
+
+//function empty(array0) {
+  //  for (i = 0; i < array0.length; i++); {
+    //    console.log(array0[i]);
+      //  if (array0[i] > 0);
+        //array0[i].pop[i];
+        //console.log('Removed item:', array0[i]);
+  //  }
+//}
+
+//console.log(empty(basket))
 
