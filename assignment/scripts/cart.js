811bee3ff7208ec4af18ab584b19ca7921531a6b
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-08 09:50:08 -0600

    added function to add item

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,13 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    console.log('New item going into the basket:', item);
+    return true;
+}
+
+console.log(addItem('socks'));
