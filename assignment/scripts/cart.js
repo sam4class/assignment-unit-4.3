1bddad361478b6d7737a89f8500e4aea8cbfdc6e
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-08 10:11:47 -0600

    added little assignment labels for each step

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,7 +2,12 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+
+//Create a variable called 'basket'
+
 let basket = [];
+
+//Create a function called 'addItem'
 
 function addItem(item) {
     basket.push(item);
@@ -15,6 +20,7 @@ console.log(addItem('t-shirt'));
 console.log(addItem('jeans'));
 console.log(addItem('undies'));
 
+//Create a function called 'listItems'
 
 function listItems(array) {
     for (let i = 0; i < array.length; i++) {
@@ -24,3 +30,7 @@ function listItems(array) {
 }
 
 console.log(listItems(basket));
+
+//Create a function called 'empty'
+
+
