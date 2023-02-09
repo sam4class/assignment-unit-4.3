7cbb80587e449970f6ff7e0e48de83aecc07276b
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-02-09 14:10:16 -0600

    made new addItems function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -40,8 +40,7 @@ function empty(array) {
     return true;
 }
 console.log(empty(basket));
-//I kept the differnt ways I tried to create the 'empty' 
-//function at the bottom of this js file
+
 
 
 //***STRETCH GOALS***
@@ -51,13 +50,14 @@ const maxItems = 5;
 //Create a function called 'isFull'
 
 function isFull(arr) {
-    if (arr.length < maxItems) {
+    if (maxItems > arr.length) {
         console.log('More room in basket. Basket full:');
         return false;
-    } else if (arr.length > maxItems) {
+    } else if (maxItems < arr.length) {
         console.log('No more will fit. Too Full!')
+        return true;
     } else {
-        console.log('Your Basket is Full');
+        console.log('Your Basket is Full.');
     }
     return true
 }
@@ -79,32 +79,21 @@ console.log(addItem('vest'));
 
 console.log(isFull(basket));
 
-function addItem(item) {
-    basket.push(item);
-    function isFull(arr) {
-        if (arr.length < maxItems) {
-            console.log('More room in basket. Basket full:');
-            return false;
-        } else if (arr.length > maxItems) {
-            console.log('No more will fit. Too Full!')
-        } else {
-            console.log('Your Basket is Full');
-        }
-        return true
+// ReCreate 'addItems' function
+
+
+function addItem2(item, array){
+    if(isFull(array) > maxItems){
+        console.log('Basket Full!!!');
+        return false;
+    }else{
+       array.push(item);
+       console.log(`Added item: ${item}`)
     }
+    return true;
 }
 
-console.log(addItem('tie'));
-
-//let itemAdd = addItem(item){
-  //  let nowItem = function isFull(){
-    //    return item;
-    //}
-    //return nowItem;
-//}
-
-//console.log(itemAdd(basket));
-
+console.log(addItem2('hoodie', basket));
 
 
 //Create a function called 'removeItem'
@@ -116,7 +105,7 @@ function removedItem(item, array){
                 array.splice(numItem);
                 return item;
             }else{
-                console.log('Not in basket');
+                console.log('Not in basket.');
             }
            return null;
     }
@@ -125,32 +114,4 @@ function removedItem(item, array){
 //console.log(removedItem('pants', basket));
 console.log(removedItem('scarf', basket));
 
-
-
-//Ways I tried first to make 'empty' function:
-
-//function empty(item){
-//  basket.pop(item);
-//  console.log('Removed all items:', item);
-//  return 'Empty Basket';
-//}
-//console.log(empty(basket));
-
-//function empty(item) {
-  //  for(i=0; i<item.lenght; i++){
-    //    basket.pop(item);
-      //  console.log('Removed item from basket:', item);
-    //}
-//}
-//console.log(empty(basket));
-
-//function empty(array0) {
-  //  for (i = 0; i < array0.length; i++); {
-    //    console.log(array0[i]);
-      //  if (array0[i] > 0);
-        //array0[i].pop[i];
-        //console.log('Removed item:', array0[i]);
-  //  }
-//}
-//console.log(empty(basket))
 
